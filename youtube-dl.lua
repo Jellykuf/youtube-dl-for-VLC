49d9e94bb858d7aff606be5a3860aa46f1024513
Jennifer <15320805+Jellykuf@users.noreply.github.com>
2018-03-02 09:15:50 +0100

    Add files via upload

---
+++ youtube-dl.lua
@@ -0,0 +1,33 @@
+local JSON = require("JSON")
+
+function probe()
+    --Trying to catch any json input from playing.
+    if string.match( vlc.path, "ttl=" ) or string.match( vlc.path, "hash=" ) or string.match( vlc.path, ".mp4" )
+    then
+        return false
+    end
+    if vlc.access == "http" or vlc.access == "https"
+    then
+        return true
+    else
+        return false
+    end  
+end
+
+function parse()
+  local url = vlc.access .. "://" .. vlc.path
+
+  local file = assert(io.popen('youtube-dl -j '..url))
+  local output = trim1(file:read('*all'))
+  file:close()
+
+  local input = JSON:decode(output)
+
+  if input then
+    return { { path = input.url; name = input.title;} }
+  end
+end
+
+function trim1(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
