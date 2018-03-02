7585477ac474638189d2cc7fafde261fed0939fb
Jennifer <15320805+Jellykuf@users.noreply.github.com>
2018-03-02 10:09:48 +0100

    Update youtube-dl.lua

--- youtube-dl.lua
+++ youtube-dl.lua
@@ -1,30 +1,37 @@
 local JSON = require("JSON")
 
 function probe()
-    --Trying to catch any json input or youtube video from playing.
-    if string.match( vlc.path, "ttl=" ) or string.match( vlc.path, "hash=" ) or string.match( vlc.path, ".mp4" ) or string.match( vlc.path, "youtube.com" )  or string.match( vlc.path, "youtu.be" )
-    then
-        return false
-    end
-    if vlc.access == "http" or vlc.access == "https"
-    then
-        return true
-    else
-        return false
-    end  
+	--Trying to catch any json input from playing.
+	if string.match( vlc.path, "ttl=" ) or 
+	string.match( vlc.path, "hash=" ) or 
+	string.match( vlc.path, ".mp4" ) or
+	string.match( vlc.path, "youtube.com" ) or
+	string.match( vlc.path, "youtu.be" ) or
+	string.match( vlc.path, "twitch.tv" ) or
+	string.match( vlc.path, "dailymotion.com" ) or
+	string.match( vlc.path, "soundcloud.com" )
+	then
+		return false
+	end
+	if vlc.access == "http" or vlc.access == "https"
+	then
+		return true
+	else
+		return false
+	end  
 end
 
 function parse()
-  local url = vlc.access .. "://" .. vlc.path
+	local url = vlc.access .. "://" .. vlc.path
 
-  local file = assert(io.popen('youtube-dl -j '..url))
-  local output = trim1(file:read('*all'))
-  file:close()
+	local file = assert(io.popen('youtube-dl -j '..url))
+	local output = trim1(file:read('*all'))
+	file:close()
 
   local input = JSON:decode(output)
 
   if input then
-    return { { path = input.url; name = input.title;} }
+	return { { path = input.url; name = input.title;} }
   end
 end
 
